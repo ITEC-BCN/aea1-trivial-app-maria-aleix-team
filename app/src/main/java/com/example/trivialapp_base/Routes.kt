7bab633e0930978a5103119431bf7197af43b6ca
Aleix <aleix.maynou.7e9@itb.cat>
2026-01-16 08:55:15 +0100

    Creem el contingut de la splash screen i fem les rutes

--- app/src/main/java/com/example/trivialapp_base/Routes.kt
+++ app/src/main/java/com/example/trivialapp_base/Routes.kt
@@ -1,5 +1,8 @@
 package com.example.trivialapp_base
 
 sealed class Routes(val route: String) {
-
+    object Screen_splash:Routes("pantalla1")
+    object Screen_menu:Routes("pantalla2")
+    object Screen_game:Routes("pantalla3")
+    object Screen_result:Routes("pantalla4")
 }
--- app/src/main/java/com/example/trivialapp_base/view/Screen_splash.kt
+++ app/src/main/java/com/example/trivialapp_base/view/Screen_splash.kt
@@ -1,9 +1,24 @@
 package com.example.trivialapp_base.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.content.MediaType.Companion.Image
+import androidx.compose.foundation.content.MediaType.Companion.Text
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.navigation.NavController
+import com.example.trivialapp_base.R
 
 @Composable
 fun SplashScreen(navController: NavController) {
-
+    Box(modifier = Modifier
+        .fillMaxSize()) {
+        Image(painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Logo",)
+        Text("Trivia Masters: Pokémon & Marvel")
+    }
 }
