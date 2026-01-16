60c534c2fe079ee257be3a99f52344ad1f0cc6e4
Aleix <aleix.maynou.7e9@itb.cat>
2026-01-16 10:17:45 +0100

    Fem la funcionalitat de la navegació

--- app/src/main/java/com/example/trivialapp_base/Routes.kt
+++ app/src/main/java/com/example/trivialapp_base/Routes.kt
@@ -1,8 +1,8 @@
 package com.example.trivialapp_base
 
 sealed class Routes(val route: String) {
-    object Screen_splash:Routes("pantalla1")
-    object Screen_menu:Routes("pantalla2")
-    object Screen_game:Routes("pantalla3")
-    object Screen_result:Routes("pantalla4")
+    object Screensplash:Routes("pantalla1")
+    object Screenmenu:Routes("pantalla2")
+    object Screengame:Routes("pantalla3")
+    object Screenresult:Routes("pantalla4")
 }
