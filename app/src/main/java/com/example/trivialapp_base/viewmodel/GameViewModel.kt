e0e8ae9ea623f4da7083313317100747dd73021a
Aleix <aleix.maynou.7e9@itb.cat>
2026-01-29 09:48:18 +0100

    Feature. Afegim preguntes de dificultad extrema

--- app/src/main/java/com/example/trivialapp_base/viewmodel/GameViewModel.kt
+++ app/src/main/java/com/example/trivialapp_base/viewmodel/GameViewModel.kt
@@ -7,7 +7,9 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.trivialapp_base.Routes
 import com.example.trivialapp_base.model.Pregunta
+import com.example.trivialapp_base.model.ProveedorPreguntas
 
 class GameViewModel : ViewModel() {
     private var preguntasPartida: List<Pregunta> = emptyList()
@@ -32,27 +34,97 @@ class GameViewModel : ViewModel() {
     var dificultadSeleccionada by mutableStateOf("Facil")
         private set
 
+    var categoriaSeleccionada by mutableStateOf("General")
+        private set
+
     private var timer: CountDownTimer? = null
     private val TIEMPO_POR_PREGUNTA = 10000L // 10 segons
 
     fun setDificultad(dificultad: String) {
         dificultadSeleccionada = dificultad // Sense .value!
     }
+
+    fun setCategoria(categoria: String) {
+        categoriaSeleccionada = categoria // Sense .value!
+    }
     fun iniciarJuego() {
+        if (categoriaSeleccionada!="General") {
+            preguntasPartida = ProveedorPreguntas
+                .obtenerPreguntas()
+                .filter { it.dificultad == dificultadSeleccionada && it.categoria == categoriaSeleccionada}
+                .shuffled()
+                .take(10)
+        }
+        else{
+            preguntasPartida = ProveedorPreguntas
+                .obtenerPreguntas()
+                .filter { it.dificultad == dificultadSeleccionada }
+                .shuffled()
+                .take(10)
+        }
+        indicePreguntaActual = 0
+        preguntaActual = preguntasPartida[indicePreguntaActual]
+        puntuacion = 0
+        juegoTerminado = false
+        respuestasMezcladas = listOf(
+            preguntaActual!!.respuesta1,
+            preguntaActual!!.respuesta2,
+            preguntaActual!!.respuesta3,
+            preguntaActual!!.respuesta4,
+        ).shuffled()
+        iniciarTimer()
     }
 
     private fun cargarSiguientePregunta() {
+        preguntaActual = preguntasPartida[indicePreguntaActual]
+        respuestasMezcladas = listOf(
+            preguntaActual!!.respuesta1,
+            preguntaActual!!.respuesta2,
+            preguntaActual!!.respuesta3,
+            preguntaActual!!.respuesta4,
+        ).shuffled()
+        iniciarTimer()
     }
 
-    fun responderPregunta(respuestaUsuario: String) {
+    fun responderPregunta(respuestaUsuario: String){
+        if (respuestaUsuario == preguntaActual!!.respuestaCorrecta){
+            puntuacion++
+        }
+        avanzarRonda()
+        cargarSiguientePregunta()
     }
 
-    private fun avanzarRonda() {
+    private fun avanzarRonda(){
+        if (indicePreguntaActual==9) juegoTerminado = true
+        else indicePreguntaActual++
     }
 
-    private fun iniciarTimer() {
+    private fun iniciarTimer(){
+            timer?.cancel()
+            timer = object : CountDownTimer(TIEMPO_POR_PREGUNTA, 100) {
+                override fun onTick(millisUntilFinished: Long) {
+                    tiempoRestante = millisUntilFinished.toFloat() / TIEMPO_POR_PREGUNTA
+                }
+
+                override fun onFinish(){
+                    tiempoRestante = 0f
+
+                    avanzarRonda()
+
+                    if(indicePreguntaActual < 9){
+
+                        cargarSiguientePregunta()
+                    }
+                    else {
+                        juegoTerminado = true
+                    }
+                }
+            }.start()
+        }
+
+        override fun onCleared(){
+            super.onCleared()
+            timer?.cancel()
+        }
     }
 
-    override fun onCleared() {
-    }
-}
